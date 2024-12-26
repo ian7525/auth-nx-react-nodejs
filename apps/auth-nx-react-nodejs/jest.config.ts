73f7587e9c0be0006ed3e7c4ebb44f14bc090c83
ian7525 <ian7525@gmail.com>
2024-12-26 21:06:05 +0800

    Initial commit

---
+++ apps/auth-nx-react-nodejs/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'auth-nx-react-nodejs',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/auth-nx-react-nodejs',
+};
